a0a862f80dd490fa8cf1efbc3db76e6a83a4e42f
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2021-07-12 13:44:27 +0100

    add(pwm_db_api/schema.sql)

---
+++ pwm_db_api/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS user_t (
+   id SERIAL PRIMARY KEY,  
+   username VARCHAR unique not null,
+   password VARCHAR not null,
+   key VARCHAR not null -- encryption and decryption user key.
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+   session_id VARCHAR PRIMARY KEY,
+   jwt_token VARCHAR not null,
+   user_id INTEGER,
+   revoked boolean,
+   FOREIGN KEY(user_id) REFERENCES user_t(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS passwords (
+   id SERIAL PRIMARY KEY,
+   user_id INTEGER not null,
+   password VARCHAR not null,
+   category TEXT,
+   site TEXT,
+   FOREIGN KEY(user_id) REFERENCES user_t(id)
+);
+
+CREATE TABLE IF NOT EXISTS server (
+   server_key VARCHAR, 
+   auth_server_key VARCHAR
+);
