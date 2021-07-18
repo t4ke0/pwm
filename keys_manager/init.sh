2bdc128a8d36f5cc048b82a4e0452f4fda3e6b83
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2021-07-18 12:15:49 +0100

    remove init.sh

--- keys_manager/init.sh
+++
@@ -1,6 +0,0 @@
-export WORD_LIST_PATH=common/words.txt
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=securepassword
-export POSTGRES_HOST=localhost
-export POSTGRES_DB=postgres
-export TEST=true
