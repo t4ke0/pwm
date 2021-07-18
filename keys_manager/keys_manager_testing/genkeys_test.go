d76a06e9f67edb75e41c255ff1a8a56f9041b836
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2021-07-18 11:53:35 +0100

    update(keys_manager_testing) unit testing.

--- keys_manager/keys_manager_testing/genkeys_test.go
+++ keys_manager/keys_manager_testing/genkeys_test.go
@@ -1,79 +1,68 @@
 package keys_manager_testing
 
 import (
-	"context"
 	"testing"
 
-	"google.golang.org/grpc"
-
-	keys_manager_pb "github.com/t4ke0/pwm/keys_manager/proto"
+	"github.com/t4ke0/pwm/keys_manager/common"
 )
 
-const serverHost string = "localhost:9090"
+const wordsFilePath = "../common/words.txt"
 
-func newGRPCconn() (*grpc.ClientConn, error) {
-	opts := []grpc.DialOption{grpc.WithInsecure()}
-	conn, err := grpc.Dial(serverHost, opts...)
+var key, userKey common.Key
+
+var encryptedKey []byte
+
+func TestGenerateKey(t *testing.T) {
+	var err error
+	key, err = common.GenerateEncryptionKey(wordsFilePath, 0)
 	if err != nil {
-		return nil, err
-	}
-	return conn, nil
-}
-
-func queryKeyGen(mode keys_manager_pb.Mode) (*keys_manager_pb.KeyResponse, error) {
-	conn, err := newGRPCconn()
-	if err != nil {
-		return nil, err
-	}
-	defer conn.Close()
-
-	keyManagerClient := keys_manager_pb.NewKeyManagerClient(conn)
-	return keyManagerClient.GenKey(context.TODO(),
-		&keys_manager_pb.KeyGenRequest{
-			Mode: mode,
-		})
-}
-
-func TestGenServerKey(t *testing.T) {
-	key, err := queryKeyGen(keys_manager_pb.Mode_Server)
-	if err != nil {
-		t.Logf("generate server key [%v]", err)
+		t.Logf("Generate server Key [%v]", err)
 		t.Fail()
 		return
 	}
-	if key != nil && key.Key == "" {
-		t.Logf("server key is empty")
-		t.Fail()
-	}
-}
+	t.Logf("Generated server key [%v]", key.String())
 
-func TestGenAlreadyExistsServeKey(t *testing.T) {
-	key, err := queryKeyGen(keys_manager_pb.Mode_Server)
-	if err == nil || key != nil {
-		t.Log("couldn't catch server key conflict")
-		t.Fail()
-		return
-	}
-}
-
-func TestGenUserKey(t *testing.T) {
-	key, err := queryKeyGen(keys_manager_pb.Mode_User)
+	userKey, err = common.GenerateEncryptionKey(wordsFilePath, 0)
 	if err != nil {
-		t.Logf("couldn't generate user key [%v]", err)
+		t.Logf("Generate user key [%v]", err)
 		t.Fail()
 		return
 	}
+	t.Logf("Generated User key [%v]", userKey.String())
+}
 
+func TestEncryptUserKey(t *testing.T) {
 	if key == nil {
-		t.Logf("couldn't generate user key [key is nil]")
+		t.Log("key is nil")
 		t.Fail()
 		return
 	}
+	var err error
+	encryptedKey, err = key.Encrypt(userKey)
+	if err != nil {
+		t.Logf("encrypt user key [%v]", err)
+		t.Fail()
+		return
+	}
+	t.Logf("encrypted User key [%x]", encryptedKey)
+}
 
-	if key.Key == "" {
-		t.Logf("couldn't generate user key [key is empty]")
+func TestDecrpytUserKey(t *testing.T) {
+	if encryptedKey == nil {
+		t.Log("key is nil")
 		t.Fail()
 		return
 	}
-	t.Logf("User key [%v]", key.Key)
+	decryptedKey, err := key.Decrypt(encryptedKey)
+	if err != nil {
+		t.Logf("decrypt user key [%v]", err)
+		t.Fail()
+		return
+	}
+	if decryptedKey.String() != userKey.String() {
+		t.Log("user key is not the same after decypting.")
+		t.Fail()
+		return
+	}
+	t.Logf("decrypted User key [%v]", decryptedKey.String())
 }
