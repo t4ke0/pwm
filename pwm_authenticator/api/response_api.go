ad4382eb26ac80160aa7d38baa6aeaa66d366f74
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2021-09-05 16:08:52 +0100

    [pwm_authenticator]Add ErrResponse, AuthResponse

--- pwm_authenticator/api/response_api.go
+++ pwm_authenticator/api/response_api.go
@@ -1,1 +1,11 @@
 package api
+
+// ErrResponse
+type ErrResponse struct {
+	ErrorMessage string `json:"error_message"`
+}
+
+// AuthResponse
+type AuthResponse struct {
+	JwtToken string `json:"token"`
+}
