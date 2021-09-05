b43d9dfa1770a55f75d7c6b491cb8d6fb05840ef
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2021-09-05 16:10:22 +0100

    [pwm_authenticator] update jwt token functions
    - Add parse jwt token.
    
    - update getNewJWTtoken function.

--- pwm_authenticator/cmd/serve/jwt_token.go
+++ pwm_authenticator/cmd/serve/jwt_token.go
@@ -1,20 +1,53 @@
 package main
 
-import "github.com/dgrijalva/jwt-go/v4"
+import (
+	"fmt"
+	"log"
 
-// TokenClaims is the information that a JWT token will hold.
-type TokenClaims struct {
-	Username     string
-	SessionID    string
-	SymmetricKey string
+	"github.com/dgrijalva/jwt-go/v4"
+)
+
+type tokenClaims struct {
+	UserID       int    `json:"user_id"`
+	Username     string `json:"username"`
+	SessionID    string `json:"session_id"`
+	SymmetricKey string `json:"symmetrickey"`
 }
 
-// GetNewJWTtoken get a signed JWT token.
-func GetNewJWTtoken(authServerKey []byte, claims TokenClaims) (string, error) {
+func getNewJWTtoken(authServerKey []byte, claims tokenClaims) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userid":       claims.UserID,
 		"username":     claims.Username,
-		"sessionID":    claims.SessionID,
-		"SymmetricKey": claims.SymmetricKey,
+		"sessionid":    claims.SessionID,
+		"symmetrickey": claims.SymmetricKey,
 	})
 	return token.SignedString(authServerKey)
+}
+
+func parseJWTtoken(tokenString string, authServerKey []byte) (tokenClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return authServerKey, nil
+	})
+
+	if err != nil {
+		return tokenClaims{}, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return tokenClaims{}, fmt.Errorf("type assertion error token claims")
+	}
+	if !token.Valid {
+		return tokenClaims{}, fmt.Errorf("token not valid")
+	}
+	log.Printf("debug: %v", claims)
+	return tokenClaims{
+		UserID:       int(claims["userid"].(float64)),
+		Username:     claims["username"].(string),
+		SessionID:    claims["sessionid"].(string),
+		SymmetricKey: claims["symmetrickey"].(string),
+	}, nil
 }
