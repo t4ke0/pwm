5bb27fb3f0c230a29e317386c834d1a6d6f0b2c4
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2021-09-05 16:08:07 +0100

    [pwm_authenticator] add AuthRequest
    add Byte method for Field type.

--- pwm_authenticator/api/request_api.go
+++ pwm_authenticator/api/request_api.go
@@ -11,6 +11,12 @@ type RegisterRequest struct {
 	Email    Field `json:"email"`
 }
 
+// AuthRequest
+type AuthRequest struct {
+	Username Field `json:"username"`
+	Password Field `json:"password"`
+}
+
 // Field string type alias that represents json body field.
 type Field string
 
@@ -25,4 +31,9 @@ func (f Field) IsEmpty() bool {
 // String returns field as a string.
 func (f Field) String() string {
 	return string(f)
+}
+
+// Byte returns field as a []byte.
+func (f Field) Byte() []byte {
+	return []byte(f.String())
 }
