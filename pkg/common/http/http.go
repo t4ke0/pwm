342b86656697316da6e0fd79fdb3c0c937a44b25
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2021-11-21 16:14:49 +0100

    [pkg] update CORS middleware

--- pkg/common/http/http.go
+++ pkg/common/http/http.go
@@ -6,14 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// TODO: handle custom headers to expose. if there is any .
-
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, PATCH, POST, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, accept, Content-Length")
-		// c.Writer.Header().Set("Access-Control-Expose-Headers", "")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, accept, Content-Length, token")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "token")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
 		}
