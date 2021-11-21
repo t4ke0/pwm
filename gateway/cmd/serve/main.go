923bcb3218665e6d73bf90d0b871772c969974b5
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2021-11-21 13:53:15 +0100

    [gateway] add gateway REST api service

---
+++ gateway/cmd/serve/main.go
@@ -0,0 +1,268 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+
+	pwm_manager_pb "github.com/t4ke0/pwm/pwm_manager/proto"
+
+	"github.com/t4ke0/pwm/gateway/pkg/http/api"
+	http_toolkit "github.com/t4ke0/pwm/pkg/common/http"
+)
+
+var pwmManagerHostname string = os.Getenv("PWM_MANAGER_HOSTNAME")
+
+type gRPCconnection struct {
+	dialConn *grpc.ClientConn
+	conn     interface{}
+}
+
+func dialPWMmanager() (*gRPCconnection, error) {
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	clientConn, err := grpc.Dial(pwmManagerHostname, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	conn := pwm_manager_pb.NewManagerClient(clientConn)
+
+	return &gRPCconnection{
+		dialConn: clientConn,
+		conn:     conn,
+	}, nil
+}
+
+func handleEndpointError(c *gin.Context) {
+	if r := recover(); r != nil {
+		c.String(http.StatusInternalServerError, "Error: %v", r)
+		return
+	}
+}
+
+func authorize() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var token struct {
+			JwtToken string `json:"jwt_token"`
+		}
+		if err := c.BindJSON(&token); err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+		}
+		if token.JwtToken == "" {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+		c.Next()
+	}
+}
+
+func main() {
+
+	engine := gin.Default()
+	engine.Use(http_toolkit.CorsMiddleware())
+
+	r := http_toolkit.GetBasePATHroute(engine)
+	r.Use(authorize())
+
+	{
+		r.GET("/list/passwords", func(c *gin.Context) {
+			var req api.ListPasswordRequest
+			if err := c.BindJSON(&req); err != nil {
+				c.Status(http.StatusBadRequest)
+				return
+			}
+
+			defer handleEndpointError(c)
+			grpcConnection, err := dialPWMmanager()
+			if err != nil {
+				panic(err)
+			}
+			defer grpcConnection.dialConn.Close()
+
+			client, ok := grpcConnection.conn.(pwm_manager_pb.ManagerClient)
+			if !ok {
+				panic("gRPCconnection.conn: type assertion error")
+			}
+			ctx, cancel := context.WithTimeout(context.TODO(), time.Second*10)
+			defer cancel()
+			userPasswords, err := client.GetPasswords(ctx, &pwm_manager_pb.GetPasswordsRequest{
+				JwtToken: req.JWTtoken,
+			})
+			if err != nil {
+				panic(err)
+			}
+			var resp []api.ListPasswordResponse
+			for _, p := range userPasswords.Passwords {
+				resp = append(resp, api.ListPasswordResponse{
+					PasswordID:        int(p.PasswordID),
+					Username:          p.Data.Username,
+					PlainTextPassword: p.Data.ClearTextPassword,
+					Category:          p.Data.Category,
+					Site:              p.Data.Site,
+				})
+			}
+			c.JSON(http.StatusOK, resp)
+		})
+
+		r.POST("/add/password", func(c *gin.Context) {
+			var req api.StorePasswordRequest
+			if err := c.BindJSON(&req); err != nil {
+				c.Status(http.StatusBadRequest)
+			}
+
+			defer handleEndpointError(c)
+			grpcConnection, err := dialPWMmanager()
+			if err != nil {
+				panic(err)
+			}
+			defer grpcConnection.dialConn.Close()
+
+			client, ok := grpcConnection.conn.(pwm_manager_pb.ManagerClient)
+			if !ok {
+				panic("grpcConnection.conn: type assertion error")
+			}
+			ctx, cancel := context.WithTimeout(context.TODO(), time.Second*10)
+			defer cancel()
+			_, err = client.StorePassword(ctx, &pwm_manager_pb.ManagerRequest{
+				JwtToken: req.JWTtoken,
+				Password: &pwm_manager_pb.PasswordData{
+					ClearTextPassword: req.PlainTextPassword,
+					Username:          req.Username,
+					Category:          req.Category,
+					Site:              req.Site,
+				},
+			})
+			if err != nil && err.Error() == "record already exists" {
+				c.Status(http.StatusConflict)
+				return
+			}
+			if err != nil {
+				panic(err)
+			}
+			c.Status(http.StatusCreated)
+		})
+
+		r.PATCH("/update/password", func(c *gin.Context) {
+			var req api.UpdateUserItemsRequest
+			if err := c.BindJSON(&req); err != nil {
+				c.Status(http.StatusBadRequest)
+				return
+			}
+
+			defer handleEndpointError(c)
+
+			grpcConn, err := dialPWMmanager()
+			if err != nil {
+				panic(err)
+			}
+			defer grpcConn.dialConn.Close()
+
+			client, ok := grpcConn.conn.(pwm_manager_pb.ManagerClient)
+			if !ok {
+				panic("pwm_manager_pb.ManagerClient type assertion error")
+			}
+
+			ctx, cancel := context.WithTimeout(context.TODO(), time.Second*10)
+			defer cancel()
+
+			var values []string
+			var modes []pwm_manager_pb.ItemToUpdate
+			for _, n := range req.Items {
+				if !n.Item.Validate() {
+					c.String(http.StatusBadRequest, "%v is not a valid cred item", n.Item)
+				}
+				modes = append(modes, pwm_manager_pb.ItemToUpdate(pwm_manager_pb.ItemToUpdate_value[n.Item.String()]))
+				values = append(values, n.Value)
+			}
+
+			if _, err := client.UpdatePassword(ctx, &pwm_manager_pb.ManagerUpdateRequest{
+				JwtToken:   req.JwtToken,
+				PasswordID: int64(req.PasswordID),
+				Mode:       modes,
+				Value:      values,
+			}); err != nil {
+				panic(err)
+			}
+
+			c.Status(http.StatusOK)
+		})
+
+		r.DELETE("/delete/password", func(c *gin.Context) {
+			var req api.DeleteUserCredRequest
+			if err := c.BindJSON(&req); err != nil {
+				c.Status(http.StatusBadRequest)
+				return
+			}
+
+			defer handleEndpointError(c)
+
+			grpcConn, err := dialPWMmanager()
+			if err != nil {
+				panic(err)
+			}
+			defer grpcConn.dialConn.Close()
+
+			client, ok := grpcConn.conn.(pwm_manager_pb.ManagerClient)
+			if !ok {
+				panic("/delete/password : type assertion error pwm_manager_pb.ManagerClient")
+			}
+
+			ctx, cancel := context.WithTimeout(context.TODO(), time.Second*10)
+			defer cancel()
+
+			_, err = client.DeletePasswords(ctx, &pwm_manager_pb.DeletePasswordRequest{
+				JwtToken:   req.JwtToken,
+				PasswordID: int64(req.PasswordID),
+			})
+
+			if err != nil {
+				panic(err)
+			}
+
+			c.Status(http.StatusOK)
+		})
+
+		r.GET("/gen/password", func(c *gin.Context) {
+			var req api.GeneratePasswordRequest
+			if err := c.BindJSON(&req); err != nil {
+				c.Status(http.StatusBadRequest)
+				return
+			}
+
+			defer handleEndpointError(c)
+			grpcConn, err := dialPWMmanager()
+			if err != nil {
+				panic(err)
+			}
+			defer grpcConn.dialConn.Close()
+
+			conn, ok := grpcConn.conn.(pwm_manager_pb.ManagerClient)
+			if !ok {
+				panic("type assertion")
+			}
+
+			ctx, cancel := context.WithTimeout(context.TODO(), time.Second*10)
+			defer cancel()
+
+			mode := pwm_manager_pb.PasswordMode(pwm_manager_pb.PasswordMode_value[req.PasswordComplexity.String()])
+
+			generatedPassword, err := conn.GeneratePassword(ctx, &pwm_manager_pb.GeneratePasswordRequest{
+				Length: req.PasswordLength,
+				Mode:   mode,
+			})
+
+			if err != nil {
+				panic(err)
+			}
+
+			c.JSON(http.StatusCreated, api.GeneratePasswordResponse{
+				GeneratedPassword: generatedPassword.Password,
+			})
+		})
+
+	}
+	engine.Run(":6969")
+}
---
+++ gateway/pkg/http/api/api.go
@@ -0,0 +1,91 @@
+package api
+
+// ListPasswordRequest JSON request to list user passwords.
+type ListPasswordRequest struct {
+	JWTtoken string `json:"jwt_token"`
+}
+
+// ListPasswordResponse JSON response of list user passwords endpoint.
+type ListPasswordResponse struct {
+	PasswordID        int    `json:"password_id"`
+	Username          string `json:"username"`
+	PlainTextPassword string `json:"plaintext_password"`
+	Category          string `json:"category"`
+	Site              string `json:"site"`
+}
+
+// StorePasswordRequest JSON request to store passwords of a user.
+type StorePasswordRequest struct {
+	JWTtoken string `json:"jwt_token"`
+
+	Username          string `json:"username"`
+	PlainTextPassword string `json:"plaintext_password"`
+	Category          string `json:"category"`
+	Site              string `json:"site"`
+}
+
+// CredItem
+type CredItem string
+
+const (
+	Username CredItem = "Username"
+	Password          = "Password"
+	Category          = "Category"
+	Site              = "Site"
+)
+
+// String
+func (self CredItem) String() string {
+	return string(self)
+}
+
+// Validate
+func (self CredItem) Validate() bool {
+	if self != Username && self != Password && self != Category && self != Site {
+		return false
+	}
+	return true
+}
+
+// UpdateUserItemsRequest JSON request to update user's creds items such as
+// `password`, `site`, etc ....
+type UpdateUserItemsRequest struct {
+	JwtToken   string `json:"jwt_token"`
+	PasswordID int    `json:"password_id"`
+	Items      []struct {
+		Item  CredItem `json:"item"`
+		Value string   `json:"value"`
+	} `json:"items"`
+}
+
+// DeleteUserCredRequest
+type DeleteUserCredRequest struct {
+	JwtToken   string `json:"jwt_token"`
+	PasswordID int    `json:"password_id"`
+}
+
+// Complexity
+type Complexity string
+
+const (
+	PasswordEasy    Complexity = "Easy"
+	PasswordMedium             = "Medium"
+	PasswordComplex            = "Complex"
+)
+
+func (c Complexity) String() string {
+	return string(c)
+}
+
+// GeneratePasswordRequest
+type GeneratePasswordRequest struct {
+	JwtToken string `json:"jwt_token"`
+
+	PasswordLength     int64      `json:"password_length"`
+	PasswordComplexity Complexity `json:"password_complexity"`
+}
+
+// GeneratePasswordResponse
+type GeneratePasswordResponse struct {
+	GeneratedPassword string `json:"generated_password"`
+}
